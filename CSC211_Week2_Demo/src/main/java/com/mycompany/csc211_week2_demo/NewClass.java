cf936e39fc901ce2e019439c1d5087d7a595cf3f
moaath@alrajab.net <itlabs@WHB019_1.it.farmingdale.edu>
2021-09-09 14:25:53 -0400

    Add one dimension array with init values

--- CSC211_Week2_Demo/src/main/java/com/mycompany/csc211_week2_demo/NewClass.java
+++ CSC211_Week2_Demo/src/main/java/com/mycompany/csc211_week2_demo/NewClass.java
@@ -1,12 +1,12 @@
-
 package com.mycompany.csc211_week2_demo;
-
-
-public class NewClass {
-    
-    
+public class NewClass {   
     public static void main(String[] args) {
         
+        int[] arr= new int[5]; // One dim array
+        arr[0] = 5;
+        arr[2]=10;
+        for(int i=0; i<5 ; i++){
+            System.out.println("Arry element value: " + arr[i]);
+        } 
     }
-    
 }
